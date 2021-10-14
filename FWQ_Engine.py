64ca0ff3116d512893effa0f19ab94ba895f10df
Raul <raulbeltmarc@gmail.com>
2021-10-14 14:29:04 +0200

    Cositas cada 30 segundos

--- FWQ_Engine.py
+++ FWQ_Engine.py
@@ -1,6 +1,7 @@
 import socket 
 import sys
 import threading
+import time
 
 HEADER = 64
 PORT = 5050
@@ -10,6 +11,7 @@ ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 # TODO: Quitar en un futuro
 FIN = "FIN"
+XSEC = 30
 
 # Función que se encarga de enviar el mapa actualizado al gestor de colas
 def send(msg, client):
@@ -47,18 +49,26 @@ def start(MAX_CONEXIONES):
     print(f"[LISTENING] Servidor a la escucha en {SERVER}")
     CONEX_ACTIVAS = threading.active_count()-1
     print(f"{CONEX_ACTIVAS} / {MAX_CONEXIONES}")
+    print("wtf")
+    start = time.time()
+    print("wtf")
     while True:
-        conn, addr = server.accept()
-        CONEX_ACTIVAS = threading.active_count()
+        
+        if (round((time.time() - start)) == XSEC):
+            print("ichi bio keka")
+            input()
+            start = time.time()
+        # conn, addr = server.accept()
+        # CONEX_ACTIVAS = threading.active_count()
         if (CONEX_ACTIVAS <= MAX_CONEXIONES): 
-            thread = threading.Thread(target=handle_client, args=(conn, addr))
-            thread.start()
+            # thread = threading.Thread(target=handle_client, args=(conn, addr))
+            # thread.start()
             print(f"[CONEXIONES ACTIVAS] {CONEX_ACTIVAS}")
-            print("CONEXIONES RESTANTES PARA CERRAR EL SERVICIO", MAX_CONEXIONES-CONEX_ACTIVAS)
+            # print("CONEXIONES RESTANTES PARA CERRAR EL SERVICIO", MAX_CONEXIONES-CONEX_ACTIVAS)
         else:
             print("AFORO ALCANZADO")
-            conn.send("El parque ha alcanzado su aforo máximo, vuelve en otro momento.".encode(FORMAT))
-            conn.close()
+            # conn.send("El parque ha alcanzado su aforo máximo, vuelve en otro momento.".encode(FORMAT))
+            # conn.close()
             CONEX_ACTUALES = threading.active_count()-1
     
 ########## MAIN ##########
@@ -78,7 +88,6 @@ if  (len(sys.argv) == 6):
     server.bind(ADDR)
 
     print("[STARTING ENGINE] Servidor inicializándose...")
-    start(MAX_CONEXIONES)
-
+    start(10)
 else:
     print ("Oops!. Parece que algo falló.\nNecesito estos argumentos:<ServerIP_FAKFA> <Puerto_FAKFA> <SERVERIP_STE> <PUERTO_STE> <Nº MAX VISITANTES>")
