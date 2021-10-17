7795b2e1cc81e06fd3d2a7c6492c70bb7a23ce7a
Raul <raulbeltmarc@gmail.com>
2021-10-17 19:52:11 +0200

    pip install kafka-python

--- FWQ_Sensor.py
+++ FWQ_Sensor.py
@@ -1,20 +1,12 @@
-import socket
 import sys
 import time
+from kafka import KafkaConsumer #pip install kafka-python
 
 HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 XSEC= 3
 FIN = "FIN"
-
-def send(msg):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
     
 ########## MAIN ##########
 
@@ -28,20 +20,14 @@ if  (len(sys.argv) == 4):
     while True:
         # Cada X segundos se conecta a STE para actualizar los tiempos de espera de las atracciones
         if (round((time.time() - start)) == XSEC):
-            # TODO: SOCKETS != Streaming & QM?
-            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            client.connect(ADDR)
-            print (f"Establecida conexión con el gestor de colas [{ADDR}]")
-            # TODO: Enviar el número de personas que se encuentran en la atracción correspondiente a ID
-                ########
-            print('Enviar mensaje (Debería ser automático)')
-            msg = input()
-            print('Enviando mensaje.... ' + msg + "\n")
-            send(msg)
-            print("SERVIDOR: ", client.recv(2048).decode(FORMAT))
-                ########
+            
+            # TODO: Cómo obtener el número de personas en la ID_Atracción?
+
+            consumer = KafkaConsumer('sensor')
+            for message in consumer:
+                # TODO: Enviar el número de personas que se encuentran en la atracción correspondiente a ID
+                print(message)
             print('Finalizando conexión...')
-            client.close()
             start = time.time() # Reseteamos el timer
 else:
     print ("Oops!. Parece que algo falló. Necesito estos argumentos: <ServerIP_FAKFA> <Puerto_FAKFA> <ID_Atracción>")
