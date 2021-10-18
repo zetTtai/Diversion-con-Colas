91fc3794d4ecf056d24ddae660972f583953ec0e
Joselu21 <joseluissilvestregarcia@gmail.com>
2021-10-18 11:12:38 +0200

    A la mierda coño

--- kafka-installer.ps1
+++ kafka-installer.ps1
@@ -1,9 +1,14 @@
+$Kafka_Command_Path = ".\bin\windows\zookeper-server-start.bat"
+$Kafka_Config_File = ".\config\zookeeper.properties"
+
 C:
 Set-Location /
 New-Item kafka -ItemType Directory
 Set-Location kafka
-Invoke-WebRequest https://archive.apache.org/dist/kafka/2.8.1/kafka_2.13-2.8.1.tgz -OutFile kafka.tgz
-tar -xzf kafka.tgz
-Get-ChildItem -Path kafka_2.13-2.8.1 -Recurse -File | Move-Item -Destination .
-Remove-Item kafka.tgz
-Remove-Item kafka_2.13-2.8.1
+Invoke-WebRequest https://github.com/zetTtai/Diversion-con-Colas/blob/Kafka-Installer/Kafka.zip?raw=true -OutFile kafka.zip
+Expand-Archive -Path kafka.zip -DestinationPath .
+Get-ChildItem -Path Kafka -Recurse -File | Move-Item -Destination .
+#Remove-Item kafka.tgz
+Remove-Item Kafka
+cmd.exe         .\bin\windows\zookeper-server-start.bat                 .\config\zookeeper.properties
+Invoke-Command  .\bin\windows\kafka-server-start.bat     -ArgumentList  .\config\server.properties
