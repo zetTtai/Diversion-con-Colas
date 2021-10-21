5a52931b2b4124c1161005cd16c1ac6edaeeadcc
Raul <raulbeltmarc@gmail.com>
2021-10-21 17:54:46 +0200

    De donde sale el tiempo de ciclo?

--- FWQ_WaitingTimeServer.py
+++ FWQ_WaitingTimeServer.py
@@ -1,73 +1,82 @@
 import socket 
 import sys
 import threading
-import time
+import fileinput
 
 HEADER = 64
 # TODO: cambiar?
-PORT = 5050
-# TODO: cambiar?
 SERVER = socket.gethostbyname(socket.gethostname())
-ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
-# TODO: Quitar en un futuro
-FIN = "FIN"
+FILE = "DATA.txt"
+
+# Función para calcular el timepo de espera
+def calculateWaitTime(tiempo, visitanes):
+    return 0
+
+# Función que escribe en el fichero donde se guardan los datos de las atracciones
+def writeFile(msg):
+    # Cada línea corresponde a una atracción y cada línea tiene el formato de: ID tiempo_ciclo nº_visitantes
+    # Si ya existía entonces se actualiza la linea, en caso contrario se añade al final
+    msg = msg.split(' ')
+    fichero = open(FILE, "r")
+    list_of_lines = fichero.readlines()
+    fichero.close()
+
+    existe= False
+    for i, line in enumerate(list_of_lines):
+        if line.startswith(msg[0]):
+            existe= True
+            list_of_lines[i] = msg[0] + ' ' + msg[1] + '\n'
+    
+    fichero = open(FILE, "w")
+    if(existe):
+        fichero.writelines(list_of_lines)
+    else:
+        fichero.write(msg[0] + ' ' + msg[1] + '\n')
+    fichero.close()
 
 
-# Función que se encarga de enviar el mapa actualizado al gestor de colas
-def send(msg, client):
-    message = msg.encode(FORMAT)
-    msg_length = len(message)
-    send_length = str(msg_length).encode(FORMAT)
-    send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
-    client.send(message)
+# Función que devuelve un string con todos los tiempos guardados en el fichero
+def readFile():
+    fichero = open(FILE, 'r')
+    list_of_lines = fichero.readlines()
+    fichero.close()
+    res= ""
+    for line in list_of_lines:
+        line.split()
+        # ID-TiempoEspera ID-TiempoEspera ...
+        res += line[0] + '-' + calculateWaitTime(line[1], line[2]) + ' '
+    return res
+
 
 # Recibimos petición de Engine
-def handle_client(conn, addr, PORT_ENGINE):
+def handle_client(conn, addr):
     print(f"[NUEVA CONEXION] {addr} connected.")
-    # Comprobamos que quien se conecta es Engine
-    if(conn.port == PORT_ENGINE):
-        while True:
-            # msg contiene el movimiento realizado por el Visitante
-            msg_length = conn.recv(HEADER).decode(FORMAT)
-            if msg_length:
-                msg_length = int(msg_length)
-                msg = conn.recv(msg_length).decode(FORMAT)
-                print(f" He recibido de Engine [{addr}] el mensaje:\n{msg}")
-                # TODO: Enviar a Engine los nuevos tiempo de espera
-                
-            # Terminamos de enviar la información a Engine sobre los tiempo de espera
-            print("Fin de la conexión")
-            x = input()
-            break;
-        conn.close()
-    else:
-        conn.send(f"ERROR: Tu puerto [{conn.port}] no es válido.".encode(FORMAT))
-        conn.close()
-
-# TODO: No se conecta por sockets
-# def connectToKAFKA(ADDR_KAFKA, msg): # (CLIENTE de KAFKA)
-#     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     client.connect(ADDR_KAFKA)
-#     print (f"Establecida conexión en [{ADDR}]")
-#     # TODO: Enviar a Kafka el mapa actualizado
-#     while msg != FIN:
-#         print("SERVIDR: ", client.recv(2048).decode(FORMAT))
-#         msg = input()
-#     send(FIN, client)
-#     client.close()
+    print("Engine se ha conectado")
+    # TODO: Enviar a Engine los nuevos tiempo de espera
+    print("Extrayendo datos del fichero")
+    msg = readFile()
+    print("Enviando datos a Engine")
+    conn.send(f"{msg}".encode(FORMAT))
+        
+    # Terminamos de enviar la información a Engine sobre los tiempo de espera
+    print("Fin de la conexión")
+    x = input()
+    conn.close()
 
 
-def start(ADDR_KAFKA, PORT_ENGINE): # (SERVIDOR DE KAFKA)
+def start(ADDR_KAFKA):
     server.listen()
     print(f"[LISTENING] Servidor a la escucha en {SERVER}")
 
     # Escuchamos indefinidamente
     while True:
         conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn, addr, PORT_ENGINE))
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
+        # TODO: Gestionar los mensajes recibidos por el gestor de colas (Sensores)
+        msg=""
+        writeFile(msg)
     
 
 ########## MAIN ##########
@@ -77,12 +86,12 @@ if  (len(sys.argv) == 4):
     PORT_FAKFA = int(sys.argv[2])
     ADDR_KAFKA = (SERVER_FAKFA, PORT_FAKFA)
 
-    PORT_ENGINE = int(sys.argv[3])
-
+    PORT = int(sys.argv[3])
+    ADDR = (SERVER, PORT)
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(ADDR)
 
     print("[STARTING WTS] Servidor inicializándose...")
-    start(ADDR_KAFKA, PORT_ENGINE)
+    start(ADDR_KAFKA)
 else:
     print ("Oops!. Parece que algo falló.\nNecesito estos argumentos:<ServerIP_FAKFA> <Puerto_FAKFA> <Puerto_Escucha>")
