3ea719e3dbbdee86d21c491319243787c37c42cb
Raul <raulbeltmarc@gmail.com>
2021-10-22 10:35:19 +0200

    Quizá toque crear una tabla mapa para controlar el aforo

--- FWQ_Engine.py
+++ FWQ_Engine.py
@@ -4,6 +4,8 @@ import threading
 import time
 import sqlite3
 import traceback
+from kafka import KafkaConsumer
+from kafka import KafkaProducer
 
 HEADER = 64
 # TODO: cambiar?
@@ -13,8 +15,6 @@ SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 XSEC = 30
-# TODO: Quitar en un futuro
-FIN = "FIN"
 
 
 # Función que se encarga de enviar el mapa actualizado al gestor de colas
@@ -26,8 +26,7 @@ def send(msg, client):
     client.send(send_length)
     client.send(message)
 
-# Función que se encarga de actualizar el mapa según el movimiento recibido por argumentos
-def handle_client(conn, addr, ADDR_KAFKA):
+def handle_client(conn, addr):
     print(f"[NUEVA CONEXION] {addr} connected.")
     while True:
         # msg contiene el movimiento realizado por el Visitante
@@ -38,10 +37,7 @@ def handle_client(conn, addr, ADDR_KAFKA):
             print(f" He recibido del servidor de colas [{addr}] el mensaje:\n{msg}")
 
             # TODO: Actualizar mapa
-            # TODO: conn y addr tienen que ser los de FAKFA?
             print("Enviando mapa actualizado...")
-            # connectToKAFKA(ADDR_KAFKA, msg)
-            # conn.send(f"{msg}".encode(FORMAT))
         break
     print("ADIOS. TE ESPERO EN OTRA OCASION")
     conn.close()
@@ -73,19 +69,20 @@ def connectToSTE(ADDR_STE): # (CLIENTE de STE)
     client.connect(ADDR_STE)
     print (f"Establecida conexión en [{ADDR}]")
     # TODO: Recibir mensaje de STE y actualizar tiempos de espera de la Base de Datos
-    while True:
-        # print("SERVIDR: ", client.recv(2048).decode(FORMAT))
-        actualizarTiemposEspera(client.recv(2048).decode(FORMAT))
-        break;
-    send(FIN, client)
+    # print("SERVIDR: ", client.recv(2048).decode(FORMAT))
+    actualizarTiemposEspera(client.recv(2048).decode(FORMAT))
     client.close()
 
+
+def updateMap(msg):
+    return ""
+
 # Engine empieza a escuchar al gestor de colas (Kafka)
-def start(ADDR_KAFKA, ADDR_STE, MAX_CONEXIONES): # (SERVIDOR DE KAFKA)
+def start(SERVER_KAFKA, PORT_KAFKA, MAX_CONEXIONES): # (SERVIDOR DE KAFKA)
     server.listen()
     print(f"[LISTENING] Servidor a la escucha en {SERVER}")
     CONEX_ACTIVAS = threading.active_count()-1
-    print(f"{CONEX_ACTIVAS} / {MAX_CONEXIONES}")
+    # print(f"{CONEX_ACTIVAS} / {MAX_CONEXIONES}")
     start = time.time()
     while True:
         # Cada X segundos se conecta a STE para actualizar los tiempos de espera de las atracciones
@@ -93,25 +90,34 @@ def start(ADDR_KAFKA, ADDR_STE, MAX_CONEXIONES): # (SERVIDOR DE KAFKA)
             connectToSTE(ADDR_STE)
             start = time.time() # Reseteamos el timer
 
-        conn, addr = server.accept()
-        CONEX_ACTIVAS = threading.active_count()
-        if (CONEX_ACTIVAS <= MAX_CONEXIONES): 
-            thread = threading.Thread(target=handle_client, args=(conn, addr, ADDR_KAFKA))
-            thread.start()
-            print(f"[CONEXIONES ACTIVAS] {CONEX_ACTIVAS}")
-            print("CONEXIONES RESTANTES PARA CERRAR EL SERVICIO", MAX_CONEXIONES-CONEX_ACTIVAS)
-        else:
-            print("AFORO ALCANZADO")
-            conn.send("El parque ha alcanzado su aforo máximo, vuelve en otro momento.".encode(FORMAT))
-            conn.close()
-            CONEX_ACTUALES = threading.active_count()-1
+        # Recibimos un movimiento
+        consumer=KafkaConsumer('engine',bootstrap_servers=f'{SERVER_KAFKA}:{PORT_KAFKA}',auto_offset_reset='earliest')
+        for message in consumer:
+            print(message)
+            # TODO: Actualizar mapa
+            map = updateMap(message)
+            producer= KafkaProducer(bootstrap_servers=f'{SERVER_KAFKA}:{PORT_KAFKA}')
+            # Enviamos el mapa 
+            producer.send('visitantes',map.encode(FORMAT))
+
+        # conn, addr = server.accept()
+        # CONEX_ACTIVAS = threading.active_count()
+        # if (CONEX_ACTIVAS <= MAX_CONEXIONES): 
+        #     thread = threading.Thread(target=handle_client, args=(conn, addr))
+        #     thread.start()
+        #     print(f"[CONEXIONES ACTIVAS] {CONEX_ACTIVAS}")
+        #     print("CONEXIONES RESTANTES PARA CERRAR EL SERVICIO", MAX_CONEXIONES-CONEX_ACTIVAS)
+        # else:
+        #     print("AFORO ALCANZADO")
+        #     conn.send("El parque ha alcanzado su aforo máximo, vuelve en otro momento.".encode(FORMAT))
+        #     conn.close()
+        #     CONEX_ACTUALES = threading.active_count()-1
     
 ########## MAIN ##########
 
 if  (len(sys.argv) == 6):
-    SERVER_FAKFA = sys.argv[1]
-    PORT_FAKFA = int(sys.argv[2])
-    ADDR_KAFKA = (SERVER_FAKFA, PORT_FAKFA)
+    SERVER_KAFKA = sys.argv[1]
+    PORT_KAFKA = int(sys.argv[2])
 
     SERVER_STE = sys.argv[3]
     PORT_STE = int(sys.argv[4])
@@ -123,6 +129,6 @@ if  (len(sys.argv) == 6):
     server.bind(ADDR)
 
     print("[STARTING ENGINE] Servidor inicializándose...")
-    start(ADDR_KAFKA, ADDR_STE, MAX_CONEXIONES)
+    start(SERVER_KAFKA, PORT_KAFKA, ADDR_STE, MAX_CONEXIONES)
 else:
     print ("Oops!. Parece que algo falló.\nNecesito estos argumentos:<ServerIP_FAKFA> <Puerto_FAKFA> <SERVERIP_STE> <PUERTO_STE> <Nº MAX VISITANTES>")
