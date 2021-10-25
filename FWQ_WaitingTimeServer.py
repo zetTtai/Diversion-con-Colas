4ce16fcc15189518ae9447e671d74b825eaa6db0
Raul <raulbeltmarc@gmail.com>
2021-10-25 17:03:38 +0200

    Ahora STE envía un JSON a Engine

--- FWQ_WaitingTimeServer.py
+++ FWQ_WaitingTimeServer.py
@@ -1,6 +1,7 @@
 import socket 
 import sys
 import threading
+import json
 import random
 from kafka import KafkaConsumer
 
@@ -15,29 +16,30 @@ CAPACITY = []
 def updateFile(msg):
     # Cada línea corresponde a una atracción y cada línea tiene el formato de: ID tiempo_ciclo nº_visitantes
     # Si ya existía entonces se actualiza la linea, en caso contrario se añade al final
-    msg = msg.split(' ') # "ID nºvisitantes suma/resta"
+    # Convertimos el mensaje a JSON
+    msg = json.loads(msg)
     fichero = open(FILE, "r")
-    list_of_lines = fichero.readlines()
+    list_of_lines = fichero.readlines() # Obtenemos todas las lineas almacenadas en el fichero
     fichero.close()
 
-    existe= False
+    existe = False
     for i, line in enumerate(list_of_lines):
-        if line.startswith(msg[0]):
-            existe= True
-            info = list_of_lines[i].split(' ') # ID tiempo_ciclo nº_visitantes
-            if msg[2] == "+":
-                visitantes = int(info[2]) + int(msg[1])# Sumamos los visitantes antiguos + los nuevos
+        if line.startswith(msg["id"]):
+            existe = True
+            info = list_of_lines[i].split(' ') # ID tiempo_ciclo capacidad nº_visitantes
+            if msg["option"] == "+":
+                visitantes = int(info[3]) + int(msg["visitantes"])# Sumamos los visitantes antiguos + los nuevos
             else:
-                visitantes = int(info[2]) - int(msg[1])# Restamos los visitantes antiguos - los nuevos
+                visitantes = int(info[3]) - int(msg["visitantes"])# Restamos los visitantes antiguos - los nuevos
                 if(visitantes < 0): 
                     visitantes = 0
-            list_of_lines[i] = msg[0] + ' ' + random.randint(10,60) + ' ' + visitantes + '\n'
+            list_of_lines[i] = info[0] + ' ' + random.randint(10,60) + ' ' + info[2] + ' ' + visitantes + '\n'
     
     fichero = open(FILE, "w")
-    if(existe):
+    if existe:
         fichero.writelines(list_of_lines)
     else:
-        fichero.write(msg[0] + ' ' + random.randint(10,60) + ' ' + msg[1] + '\n')
+        fichero.write(msg["id"] + ' ' + random.randint(10,60) + ' ' + random.randint(50, 100) + ' '+ msg["visitantes"] + '\n')
     fichero.close()
 
 
@@ -46,13 +48,19 @@ def readFile():
     fichero = open(FILE, 'r')
     list_of_lines = fichero.readlines()
     fichero.close()
-    res= ""
-    for i, line in list_of_lines: # ID tiempo_ciclo nº_visitantes
+    res= []
+    for i, line in list_of_lines: # ID tiempo_ciclo capacidad nº_visitantes
         line.split()
         # T= número de personas que hay en cola (recibidas del sensor) /número de personas que caben en cada ciclo) * tiempo de cada ciclo.
-        tiempo_espera = (line[2]/CAPACITY[i]) * line[1]
+        # TODO: Cambiar
+        tiempo_espera = (line[3]/line[2]) * line[1]
         # ID-TiempoEspera ID-TiempoEspera ...
         res += line[0] + '-' + tiempo_espera + ' '
+        item = {
+            "id": line[0],
+            "tiempo_espera" : tiempo_espera
+        }
+        res.append(item)
     return res
 
 
@@ -68,9 +76,9 @@ def handle_client(conn, addr):
         CAPACITY = capacidades.split(' ') # Guardamos en memoria una lista con las capacidades de las atracciones del parque
         
     print("Extrayendo datos del fichero")
-    msg = readFile()
+    msg = json.dumps(readFile()) # Lo convertimos a JSON
     print("Enviando datos a Engine")
-    conn.send(f"{msg}".encode(FORMAT))
+    conn.send(f"{msg}".encode(FORMAT)) # TODO: Rezar
     # Terminamos de enviar la información a Engine sobre los tiempo de espera
     print("Fin de la conexión")
     x = input()
