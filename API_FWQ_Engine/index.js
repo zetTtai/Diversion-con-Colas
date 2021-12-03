6612227262668071032d4f7b9ce515839a194e33
Raul <raulbeltmarc@gmail.com>
2021-12-03 19:33:51 +0100

    fwq/map

--- API_FWQ_Engine/index.js
+++ API_FWQ_Engine/index.js
@@ -1,15 +1,97 @@
-//Cargamos el módulo express
-var express = require('express');
-var app = express();
+const path = require('path')
+var express = require('express')
 
-//En Express asociamos un método HTTP y una URL con un callback a ejecutar
-app.get('*', function(pet, resp) {
-   //Tenemos una serie de primitivas para devolver la respuesta HTTP
-   resp.status(200);
-   resp.send('Hola soy Express'); 
+var app = express()
+app.use(express.json())
+
+//----------------------------------------------------------------------------------------------------------------
+// DATABASE
+//----------------------------------------------------------------------------------------------------------------
+
+const knex = require('knex')({ 
+    client: "sqlite3",
+    connection: {
+        filename: path.resolve(__dirname, "../db/database.db")
+    },
+    useNullAsDefault: true
+})
+
+//----------------------------------------------------------------------------------------------------------------
+// Funciones
+//----------------------------------------------------------------------------------------------------------------
+
+async function show(table, idUser) {
+   obj = {}
+   if(idUser == 0) {
+      var datos = await knex.select().table(table)
+      var cuenta = await knex.select().table(table).count('* as total')
+      obj.total = cuenta[0].total
+      obj.datos = datos
+   }
+   else {
+      var datos = await knex(table).where('id_owner', idUser)
+      var cuenta = await knex(table).where('id_owner', idUser).count('* as total')
+      obj.total = cuenta[0].total
+      obj.datos = datos
+   }
+   return obj
+}
+
+//----------------------------------------------------------------------------------------------------------------
+// Peticiones
+//----------------------------------------------------------------------------------------------------------------
+
+app.get('/fwq/visitors', async function(pet, resp) { 
+   try {
+      obj = await show('visitantes', 0)
+      resp.status(200).send(obj)
+   }
+   catch(error) {
+      console.log(error)
+      resp.status(500).send({error:error})
+   }
+})
+
+app.get('/fwq/attractions', async function(pet, resp) { 
+   try {
+      obj = await show('atracciones', 0)
+      resp.status(200).send(obj)
+   }
+   catch(error) {
+      console.log(error)
+      resp.status(500).send({error:error})
+   }
+})
+
+app.get('/fwq/logs', async function(pet, resp) { 
+   try {
+      obj = await show('registros', 0)
+      resp.status(200).send(obj)
+   }
+   catch(error) {
+      console.log(error)
+      resp.status(500).send({error:error})
+   }
+})
+
+
+app.get('/fwq/map', async function(pet, resp) {
+   try {
+      obj = {}
+      attractions = await show('atracciones', 0)
+      visitors = await show('visitantes', 0)
+      obj = {
+         "atracciones" : attractions["datos"],
+         "visitantes" : visitors["datos"]
+      }
+      resp.status(200).send(obj)
+   }
+   catch(err) {
+      console.log(error)
+      resp.status(500).send({error:error})
+   }
 });
 
-//Este método delega en el server.listen "nativo" de Node
-app.listen(3000, function () {
-   console.log("El servidor express está en el puerto 3000");
+var listener = app.listen(process.env.PORT||3000, () => {
+   console.log(`Servidor en el puerto ${listener.address().port}`);
 });
